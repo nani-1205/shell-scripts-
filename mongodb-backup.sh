47b53215da2e5c5b0e8ca64bf1d3661ea0024603
YETUKURI SAI JAGAN <nani.y9966@gmail.com>
2025-02-07 11:33:41 +0530

    v-1

---
+++ mongodb-backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Variables (modify these!)
+HOST="18.61.75.247:27017"
+USERNAME="Jagan"
+PASSWORD="Jagan@1234"
+AUTHENTICATION_DATABASE="admin"
+
+# Attempt to connect using the mongo shell
+mongo --host "$HOST" -u "$USERNAME" -p "$PASSWORD" --authenticationDatabase "$AUTHENTICATION_DATABASE" <<EOF
+  print("Successfully connected to MongoDB!");
+  db.version(); // Get the MongoDB server version
+  quit();
+EOF
+
+# Check the exit code
+if [ $? -eq 0 ]; then
+  echo "Connection test successful."
+else
+  echo "Connection test failed. Check credentials and host."
+  exit 1 # Exit with an error if the connection fails
+fi
+
+exit 0
