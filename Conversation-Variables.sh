d2648e3b135c91995438d9fbcf120737c32491b4
YETUKURI SAI JAGAN <nani.y9966@gmail.com>
2025-02-12 11:01:15 +0530

    Conversation-Variables

---
+++ Conversation-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Variables
+User1="Jagan"
+User2="Nani"
+
+echo ""$User1" :: Hello "$User2" how are you?"
+echo ""$User2" :: YA! Fine. What about you?"
+echo ""$User1" :: YA! Fine. I will call you later?"
+echo ""$User2" :: Ok bye😁"
