22f4f662bdb3efdd5518281ebb74b4f976892142
YETUKURI SAI JAGAN <nani.y9966@gmail.com>
2025-02-14 12:09:39 +0530

    load-v2-fixed

--- load_average.sh
+++ load_average.sh
@@ -3,7 +3,14 @@
 # Script to display load average
 
 echo "--- Load Average (1, 5, 15 minutes) ---"
-load_average=$(uptime | awk -F '[:,]' '{print $4 ", " $5 ", " $6}')
+load_average=$(uptime | tr ',' ' ' | awk '{
+  for (i=1; i<=NF; i++) {
+    if ($i == "load" && $(i+1) == "average:") {
+      printf "%s, %s, %s\n", $(i+2), $(i+3), $(i+4)
+      exit
+    }
+  }
+}')
 echo "$load_average"
 
 exit 0
